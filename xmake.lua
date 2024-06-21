e8ebde7f1cd8abb7ef3653cb617471aa3d6eb5d6
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-06-21 13:27:19 +0100

    add config to astc-encoder

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,8 @@ add_repositories("local-repo local_repository")
 
 set_languages("cxx17")
 
-add_requires("lzham_codec", "lzma", "zstd", "astc-encoder")
+add_requires("lzham_codec", "lzma", "zstd")
+add_requires("astc-encoder", {configs = {sse41 = true, native = true, cli = false}})
 add_requires("supercell_core")
 
 target("supercell_compression")
