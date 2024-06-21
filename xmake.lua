b358bc2f3ab595a3b0dcf14675b423b3fe15cb42
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-06-21 12:11:00 +0100

    add xmake

---
+++ xmake.lua
@@ -0,0 +1,18 @@
+add_rules("mode.debug", "mode.release")
+
+add_repositories("local-repo local_repository")
+
+set_languages("cxx17")
+
+add_requires("lzham_codec", "lzma", "zstd", "astc-encoder")
+add_requires("supercell_core")
+
+target("supercell_compression")
+    set_kind("$(kind)")
+
+    add_packages("lzham_codec", "lzma", "zstd", "astc-encoder")
+    add_packages("supercell_core")
+
+    add_files("source/**.cpp")
+    add_headerfiles("include/(**.h)")
+    add_includedirs("include")
